051ab76ca3354832aefe178635ad7b13339a5401
emclaughlin42 <eric.mclaughlin@edu.sait.ca>
2023-01-24 19:08:13 -0700

    Initial commit

---
+++ src/java/database/tables.sql
@@ -0,0 +1,25 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  Eric
+ * Created: 24-Jan-2023
+ */
+
+DROP SCHEMA IF EXISTS `helixschedulingdb`;
+CREATE SCHEMA IF NOT EXISTS `helixschedulingdb` DEFAULT CHARACTER SET latin1;
+USE `helixschedulingdb`;
+
+CREATE TABLE IF NOT EXISTS `helixschedulingdb`.`user` (
+    `userID` INT(10) NOT NULL AUTO_INCREMENT,
+    `email` VARCHAR(40) NOT NULL,
+    `firstName` VARCHAR(20) NOT NULL,
+    `lastName` VARCHAR(20) NOT NULL,
+    `password` VARCHAR(40) NOT NULL,
+    `phone` VARCHAR(10),
+    `active` BOOLEAN,
+    `public` BOOLEAN,
+    PRIMARY KEY(userID)
+);
---
+++ src/java/model/User.java
@@ -0,0 +1,168 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author Eric
+ */
+@Entity
+@Table(name = "user")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "User.findAll", query = "SELECT u FROM User u")
+    , @NamedQuery(name = "User.findByUserID", query = "SELECT u FROM User u WHERE u.userID = :userID")
+    , @NamedQuery(name = "User.findByEmail", query = "SELECT u FROM User u WHERE u.email = :email")
+    , @NamedQuery(name = "User.findByFirstName", query = "SELECT u FROM User u WHERE u.firstName = :firstName")
+    , @NamedQuery(name = "User.findByLastName", query = "SELECT u FROM User u WHERE u.lastName = :lastName")
+    , @NamedQuery(name = "User.findByPassword", query = "SELECT u FROM User u WHERE u.password = :password")
+    , @NamedQuery(name = "User.findByPhone", query = "SELECT u FROM User u WHERE u.phone = :phone")
+    , @NamedQuery(name = "User.findByActive", query = "SELECT u FROM User u WHERE u.active = :active")
+    , @NamedQuery(name = "User.findByPublic1", query = "SELECT u FROM User u WHERE u.public1 = :public1")})
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "userID")
+    private Integer userID;
+    @Basic(optional = false)
+    @Column(name = "email")
+    private String email;
+    @Basic(optional = false)
+    @Column(name = "firstName")
+    private String firstName;
+    @Basic(optional = false)
+    @Column(name = "lastName")
+    private String lastName;
+    @Basic(optional = false)
+    @Column(name = "password")
+    private String password;
+    @Column(name = "phone")
+    private String phone;
+    @Column(name = "active")
+    private Boolean active;
+    @Column(name = "public")
+    private Boolean public1;
+
+    public User() {
+    }
+
+    public User(Integer userID) {
+        this.userID = userID;
+    }
+
+    public User(Integer userID, String email, String firstName, String lastName, String password) {
+        this.userID = userID;
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.password = password;
+    }
+
+    public Integer getUserID() {
+        return userID;
+    }
+
+    public void setUserID(Integer userID) {
+        this.userID = userID;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+    public Boolean getPublic1() {
+        return public1;
+    }
+
+    public void setPublic1(Boolean public1) {
+        this.public1 = public1;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (userID != null ? userID.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof User)) {
+            return false;
+        }
+        User other = (User) object;
+        if ((this.userID == null && other.userID != null) || (this.userID != null && !this.userID.equals(other.userID))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "model.User[ userID=" + userID + " ]";
+    }
+    
+}
