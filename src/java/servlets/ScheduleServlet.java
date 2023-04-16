967509e05867dfe0b16d0770fc8568015c9de180
emclaughlin42 <eric.mclaughlin@edu.sait.ca>
2023-04-15 23:00:36 -0600

    update

--- src/java/servlets/ScheduleServlet.java
+++ src/java/servlets/ScheduleServlet.java
@@ -2,10 +2,19 @@ package servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import model.Organization;
+import model.OrganizationUser;
+import model.Schedule;
+import model.User;
+import services.OrganizationService;
+import services.OrganizationUserService;
+import services.ScheduleService;
 
 
 public class ScheduleServlet extends HttpServlet {
@@ -14,7 +23,13 @@ public class ScheduleServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-
+        ScheduleService ss = new ScheduleService();
+        HttpSession session = request.getSession();
+        
+        String orgName = request.getParameter("organization");
+        User user = (User) session.getAttribute("user");
+        
+        List<Schedule> schedule = ss.getByUser(user);
         getServletContext().getRequestDispatcher("/WEB-INF/SchedulePage.jsp").forward(request, response);
     }
 
