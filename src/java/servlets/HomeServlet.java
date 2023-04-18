f6104c2cf306bd9ee87dde25b3c3810abdfacfdd
KadenOhman <105755675+KadenOhman@users.noreply.github.com>
2023-04-18 15:53:37 -0600

    Kaden 03 07 (#20)
    * Added servlet
    
    * 1
    
    * OrgCreation
    
    * Update RergisterOrganizationServlet.java
    
    * UpdatePass
    
    Password should be updating but not for some reason
    
    * UpdatePass
    
    Working
    
    * Update AvailabilityServlet.java
    
    * Create user request
    
    allows users to request to join a organization still need to add notifications
    
    * CSS
    
    CSS
    
    * CSS
    
    CSS
    
    * Update HomePage.jsp
    
    * CSS
    
    CSS
    
    * CSS
    
    css
    
    * maybe fix 500 error
    
    * Update AvailabilityPage.jsp
    
    * CSS
    
    cSS
    
    * logout
    
    logout
    
    ---------
    
    Co-authored-by: SukhpalBahga <113125028+SukhpalBahga@users.noreply.github.com>
    Co-authored-by: emclaughlin42 <eric.mclaughlin@edu.sait.ca>
    Co-authored-by: SukhpalBahga <sukhpal.bahga@edu.sait.ca>

--- src/java/servlets/HomeServlet.java
+++ src/java/servlets/HomeServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import model.User;
+import static model.User_.firstName;
 import services.OrganizationUserService;
 import services.UserService;
 
@@ -23,6 +24,8 @@ public class HomeServlet extends HttpServlet {
         OrganizationUserService uoService = new OrganizationUserService();
         
         User user =  (User) session.getAttribute("user");
+       
+        
         
         request.setAttribute("name", user); 
         
